034e306a6320f3c3f15598ff8f38b66093348d28
Dhiman <dhiman.talapatra@gmail.com>
2024-04-12 14:07:44 +0200

    added setup vector store variable

--- src/main/java/com/dhiman/ai/bot/config/LLMConfig.java
+++ src/main/java/com/dhiman/ai/bot/config/LLMConfig.java
@@ -1,5 +1,7 @@
 package com.dhiman.ai.bot.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingClient;
 import org.springframework.ai.reader.TextReader;
@@ -15,6 +17,8 @@ import java.util.List;
 
 @Configuration
 public class LLMConfig {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
     @Value("${app.properties.setup-vector-store}")
     private boolean setupVectorStore;
     @Value("${app.properties.text-source}")
@@ -23,11 +27,14 @@ public class LLMConfig {
     @Bean
     VectorStore vectors(JdbcTemplate jdbcTemplate, EmbeddingClient embedding){
         PgVectorStore pgVectorStore = new PgVectorStore(jdbcTemplate, embedding);
+        logger.debug("setup-vector-store={}",setupVectorStore);
         if(setupVectorStore){
+            logger.debug("setting up vector store.");
             TextReader reader = new TextReader(this.textFile);
             TokenTextSplitter splitter = new TokenTextSplitter();
             List<Document> documents = splitter.apply(reader.get());
             pgVectorStore.add(documents);
+            logger.debug("setting up vector store completed.");
         }
         return pgVectorStore;
     }
